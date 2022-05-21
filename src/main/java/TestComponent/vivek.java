43b8eee91f1b432ba04898494e1f22106b6a0f10
Narra-vivek <narravivek168@gmail.com>
2022-05-21 11:38:18 +0530

    initial commit

---
+++ src/main/java/TestComponent/vivek.java
@@ -0,0 +1,13 @@
+package TestComponent;
+
+import TestLib.Common;
+
+public class vivek {
+	
+	public void navigateBrands()
+	{
+		Common.findElement("xpath", "//span[text()='Brands']").click();
+	}
+
+	
+}
